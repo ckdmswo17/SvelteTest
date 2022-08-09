18a63191a09d87682ed9bb0b2e2fb77d478c14df
ckdmswo17 <ckdmswo17@naver.com>
2022-08-09 10:25:08 +0900

    Deleted App.svelte

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import './global.css';
 
-import App from './App.svelte';
 import Hey from './Hey.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
 
 const hey = new Hey({
 	target: document.querySelector("#hey"),
 });
 
-export default app; hey;
+export default hey;
